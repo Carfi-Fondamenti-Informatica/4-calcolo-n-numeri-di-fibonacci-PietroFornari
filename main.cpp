be95f350087fd4bcff6f631c7cce242ffd90b7cb
PietroFornari <100316910+PietroFornari@users.noreply.github.com>
2022-03-30 21:54:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, p=0, s=1, t=0;
+    cin >> n;
+    if (n>=2) {
+        for (int i=1; i<=n; i++){
+            t = p+s;
+            s=p;
+            p=t;
+            cout << t << endl;
+        }
+    }else{
+        cout << "errore" << endl;
+    }
+
+    return 0;
 }
